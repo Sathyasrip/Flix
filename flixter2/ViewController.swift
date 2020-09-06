fc61dbe91b7d2e62b641b5abd43f5fe5aa2c6ef4
Sathyasrip <37474643+Sathyasrip@users.noreply.github.com>
2020-09-05 23:27:17 -0500

    Initial Commit

---
+++ flixter2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter2
+//
+//  Created by Sathya Sri Pasham on 9/5/20.
+//  Copyright © 2020 Sathya Sri Pasham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
