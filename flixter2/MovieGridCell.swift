0c37448ff1c0e0a5261eaa1093416f2d90292948
Sathyasrip <37474643+Sathyasrip@users.noreply.github.com>
2020-09-06 22:16:53 -0500

    collection view and task view added

---
+++ flixter2/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixter2
+//
+//  Created by Sathya Sri Pasham on 9/6/20.
+//  Copyright © 2020 Sathya Sri Pasham. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
--- flixter2/MoviesViewController.swift
+++ flixter2/MoviesViewController.swift
@@ -15,7 +15,7 @@ class MoviesViewController: UIViewController , UITableViewDataSource, UITableVie
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("helloo")
+       
         tableView.dataSource = self
         tableView.delegate = self
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
@@ -30,7 +30,7 @@ class MoviesViewController: UIViewController , UITableViewDataSource, UITableVie
 
             self.movies = dataDictionary["results"] as! [[String:Any]]
             self.tableView.reloadData()
-            print(dataDictionary)
+           
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
               // TODO: Reload your table view data
@@ -50,6 +50,7 @@ class MoviesViewController: UIViewController , UITableViewDataSource, UITableVie
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         let synopsis = movie["overview"] as! String
+        
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
@@ -60,14 +61,21 @@ class MoviesViewController: UIViewController , UITableViewDataSource, UITableVie
     }
     
 
-    /*
+   
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        print("loading the details")
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+   
 
 }
